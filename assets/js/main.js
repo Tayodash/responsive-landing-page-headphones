e36d46ee87f8fb7920880bb719debb0410da878e
Tayo Henrique <122104237+Tayodash@users.noreply.github.com>
2024-08-31 21:31:59 -0300

    att 31/08
    att indo para ajuste de tela para mobile e pc

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,38 @@ function scrollHeader() {
 
 // Adicionar event listener ao scroll da página para ativar a função scrollHeader
 window.addEventListener("scroll", scrollHeader);
+
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (window.scrollY >= 200) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+}
+
+// Adiciona o event listener para a rolagem da página
+window.addEventListener("scroll", scrollUp);
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
